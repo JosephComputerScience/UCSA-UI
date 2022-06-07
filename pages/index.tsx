fe4c6ef5349583e820de73613f42a595c6c12c46
Joseph Nguyen <tnguyen@Josephs-MBP.attlocal.net>
2022-06-06 23:01:28 -0500

    initial

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+// external imports
+import type { NextPage } from 'next';
+import classnames from 'classnames';
+
+const Home: NextPage = () => {
+  return <div className={classnames('text-2xl')}>hello</div>;
+};
+
+export default Home;
